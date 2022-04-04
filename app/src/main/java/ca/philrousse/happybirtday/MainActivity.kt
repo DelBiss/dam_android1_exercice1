cbf338cfd42d5d2b1335317af9fe6d598f2ab9f9
Philippe Allard-Rousse <philrousse@gmail.com>
2022-04-04 18:02:35 -0400

    Exercise2

--- app/src/main/java/ca/philrousse/happybirtday/MainActivity.kt
+++ app/src/main/java/ca/philrousse/happybirtday/MainActivity.kt
@@ -1,19 +1,12 @@
 package ca.philrousse.happybirtday
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonSimple: Button = findViewById(R.id.button)
-        buttonSimple.setOnClickListener{
-            buttonSimple.height = 200
-        }
-
 
     }
 
