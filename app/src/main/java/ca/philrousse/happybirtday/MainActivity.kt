e96a578656305bb88cd64829a7bab5aee46dec39
Philippe Allard-Rousse <philrousse@gmail.com>
2022-05-08 18:30:10 -0400

    Add ressource

--- app/src/main/java/ca/philrousse/happybirtday/MainActivity.kt
+++ app/src/main/java/ca/philrousse/happybirtday/MainActivity.kt
@@ -1,6 +1,8 @@
 package ca.philrousse.happybirtday
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun findDays(view : View){
+        val outputView = findViewById<TextView>(R.id.day_output)
+        outputView.text = getString(R.string.btn_msg)
+    }
 
 }
